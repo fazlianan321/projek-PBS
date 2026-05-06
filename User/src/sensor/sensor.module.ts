ef75fe598396d6bad371de29b62c2d74a67bc59e
M Fazli Anan <fazlianan321@gmail.com>
2026-05-06 14:04:29 +0700

    feat(sensor): setup basic module structure with controller and service

--- User/src/sensor/sensor.module.ts
+++ User/src/sensor/sensor.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { SensorService } from './sensor.service';
+import { SensorController } from './sensor.controller';
 
-@Module({})
+@Module({
+  providers: [SensorService],
+  controllers: [SensorController]
+})
 export class SensorModule {}
