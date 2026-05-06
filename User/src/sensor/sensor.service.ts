773e06db2cb6a1e889c608fab406497c9000888d
M Fazli Anan <fazlianan321@gmail.com>
2026-05-06 14:36:31 +0700

    chore(env): install jest types to resolve testing environment errors

--- User/src/sensor/sensor.service.ts
+++ User/src/sensor/sensor.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class SensorService {}
+export class SensorService {
+  constructor(private prisma: PrismaService) {}
+
+  // Commit 2: Menggunakan string untuk lahanId sesuai UUID di Lahan
+  async createData(data: { suhu: number; kelembapan: number; lahanId: string }) {
+    return this.prisma.sensorData.create({
+      data: {
+        suhu: data.suhu,
+        kelembapan: data.kelembapan,
+        lahanId: data.lahanId,
+      },
+    });
+  }
+
+  async getLatestData(lahanId: string) {
+    return this.prisma.sensorData.findFirst({
+      where: { lahanId },
+      orderBy: { createdAt: 'desc' },
+    });
+  }
+}
