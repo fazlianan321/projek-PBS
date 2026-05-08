cf313df63969594ebe27d2edea88e8165955d598
Vivi Restu Anggraini <vivi_restu_anggraini@teknokrat.ac.id>
2026-05-08 09:35:12 +0700

    feat(cms): setup main entry point to render App component

--- CMS/src/main.jsx
+++ CMS/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'                                                        // Memanggil file App.jsx yang kamu buat
+import './index.css'                                                              // Memanggil CSS Tailwind kamu
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
