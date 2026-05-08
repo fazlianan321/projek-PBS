fb10d4a18d7bbf4a00ad94a67831ec28e3005346
M Fazli Anan <fazlianan321@gmail.com>
2026-05-08 15:11:24 +0700

    fix(backend): register AuthModule in AppModule to enable login endpoint

--- User/src/app.module.ts
+++ User/src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { SensorModule } from './sensor/sensor.module';
+// 1. TAMBAHKAN IMPORT INI
+import { AuthModule } from './auth/auth.module'; 
 
 @Module({
-  imports: [PrismaModule, SensorModule],
+  // 2. MASUKKAN AuthModule KE DALAM ARRAY IMPORTS
+  imports: [PrismaModule, SensorModule, AuthModule], 
   controllers: [AppController],
   providers: [AppService],
 })
