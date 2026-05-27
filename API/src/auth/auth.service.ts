85307b425e74cabdba42838260fb0769fbc7cae9
M Fazli Anan <fazlianan321@gmail.com>
2026-05-27 20:48:58 +0700

    feat(auth): secure user login method with automated JWT payload signer

--- API/src/auth/auth.service.ts
+++ API/src/auth/auth.service.ts
@@ -8,3 +8,31 @@ export class AuthService {
     private prisma: PrismaService,
     private jwtService: JwtService,
   ) {}
+  
+  async login(email: string, pass: string) {
+    // 1. Cari user di database PostgreSQL (Port 5433)
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+    
+    if (!user || user.password !== pass) {
+      throw new UnauthorizedException('Email atau password salah, Fazli!');
+    }
+
+    // 2. Generate JWT Token jika login berhasil
+    const payload = { 
+      sub: user.id, 
+      email: user.email, 
+      role: user.role 
+    };
+    
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: {
+        id: user.id,
+        nama: user.name, // Diubah ke user.name agar sinkron dengan database
+        email: user.email,
+        role: user.role
+      }
+    };
+  }
