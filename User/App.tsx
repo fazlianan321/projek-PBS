43763ac2ee3597c720f5936a88ed30b7ddc0c795
Vivi Restu Anggraini <vivi_restu_anggraini@teknokrat.ac.id>
2026-05-27 14:05:37 +0700

    chore(frontend): import RegisterScreen component and useState hook inside App.tsx

--- User/App.tsx
+++ User/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import LoginScreen from './src/screens/LoginScreen'; // Pastikan path folder login kamu sudah sesuai
+import React, { useState } from 'react'; // <-- Ditambahkan useState untuk melacak posisi halaman
+import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 export default function App() {
   return (
