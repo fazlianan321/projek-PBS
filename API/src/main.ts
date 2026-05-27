e4925acf81775e9fbbede9f0de7395b1c39c4573
M Fazli Anan <fazlianan321@gmail.com>
2026-05-27 20:59:58 +0700

    chore(main): bind network interface to 0.0.0.0 for dual-stack IPv4 and IPv6 support

--- API/src/main.ts
+++ API/src/main.ts
@@ -8,7 +8,9 @@ async function bootstrap() {
   app.enableCors(); 
   // ---------------------------
 
-  await app.listen(process.env.PORT ?? 3000);
+  // 🟢 UBAH BARIS INI: Tambahkan '0.0.0.0' sebagai parameter kedua
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
