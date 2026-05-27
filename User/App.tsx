37a77030e4e9b8fd1b18c58565ca3ef0b8fca139
Vivi Restu Anggraini <vivi_restu_anggraini@teknokrat.ac.id>
2026-05-27 11:14:03 +0700

    feat(frontend): configure expo web support and set LoginScreen as main entry in App.tsx

--- User/App.tsx
+++ User/App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import LoginScreen from './src/screens/LoginScreen'; // Pastikan path folder login kamu sudah sesuai
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <LoginScreen />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +14,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
