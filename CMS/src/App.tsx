a12d1f087a5a2f7dab4a3dcddc062a308cd8cc78
Vivi Restu Anggraini <vivi_restu_anggraini@teknokrat.ac.id>
2026-05-30 21:00:44 +0700

    feat: replace Vite default template with DashboardOverview component

---
+++ CMS/src/App.tsx
@@ -0,0 +1,11 @@
+import DashboardOverview from './pages/DashboardOverview';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <DashboardOverview />
+    </div>
+  );
+}
+
+export default App;
