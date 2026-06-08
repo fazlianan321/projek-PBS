681c72a743959b43d595ff84195e2f5f1ecfed8a
M Fazli Anan <fazlianan321@gmail.com>
2026-06-08 12:02:23 +0700

    fix(types): resolve implicit any errors by adding strict axios interfaces

--- User/src/screens/AiDiagnosticScreen.tsx
+++ User/src/screens/AiDiagnosticScreen.tsx
@@ -8,7 +8,7 @@ import {
   ActivityIndicator, 
   Alert 
 } from 'react-native';
-import axios from 'axios';
+import axios, { InternalAxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
 const BACKEND_BASE_URL = 'http://10.0.2.2:3000';
 const REQUEST_TIMEOUT = 5000;
 const apiClient = axios.create({
